f13c8b2a0438318c4fc35362fd54a44aff1d6ade
susan castillo <castillosusana55@gmail.com>
2022-01-06 10:26:15 -0600

    add HEAD in app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,20 @@
 import '../styles/globals.css'
 import Layouts from './Layouts'
+import Head from 'next/head'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layouts>
-      <Component {...pageProps} />
-    </Layouts>
+    <>
+      <Head>
+        <title>DICOMES</title>
+        <link rel='icon' href='/utp.jpg'/>
+      </Head>
+      <Layouts>
+        <Component {...pageProps} />
+      </Layouts>
+    </>
+    
   )
   
 }
