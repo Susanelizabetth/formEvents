bd6f88934ae64cd1b944a1cd4768fc185e2044ec
susanelizabetth <castillosusana55@gmail.com>
2022-01-24 12:01:19 -0500

    frontend: addes componentes in todos

--- client/pages/todos.js
+++ client/pages/todos.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import Checkbox from './components/Checkbox';
 import Input from './components/Input';
 import StepBox from './components/StepBox';
+import Textarea from "./components/Textarea";
 
 import { checkPlan } from "./utils/utilData";
 import { checkProt } from "./utils/utilData";
 import { checkCont } from "./utils/utilData";
 import { checkAsen } from "./utils/utilData";
+import { checkPrAu } from "./utils/utilData";
+
 
 export default function Todos() {
     /*const [currentComt, setCurrentComt] = useState(0)*/
@@ -21,7 +24,7 @@ export default function Todos() {
     const [isCheckProt, setisCheckProt] = useState(checkProt)
     const [isCheckCont, setisCheckCont] = useState(checkCont)
     const [isCheckAsen, setisCheckAsen] = useState(checkAsen)
-
+    const [isCheckPrAu, setisCheckPrAu] = useState(checkPrAu)
 
     /*const setCurrent = (index) => {
         setCurrentComt(index)
@@ -34,19 +37,10 @@ export default function Todos() {
                     check: !isCheck.check})
     }
 
-    const onChangeCheckPlan = (index, ck) => {
-        isCheckPlan[index].check = !ck
+    const onChangeCheck = (i, d, c) => {
+       d[i].check = !c
     }
-    const onChangeCheckOth = (index, ck) => {
-        isCheckProt[index].check = !ck
-    }
-    const onChangeCheckCont = (index, ck) => {
-        isCheckCont[index].check = !ck
-    }
-    const onChangeCheckAsen = (index, ck) => {
-        isCheckAsen[index].check = !ck
-    }
-   
+
     /*function ShowComponent (i) {
         
         switch (i) {
@@ -97,11 +91,9 @@ export default function Todos() {
                             <label class="label">Planeación y organización del evento:</label>
                             {isCheckPlan.map((e, i) =>{
                                 return(
-                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheckPlan(i, e.check)} />
+                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheck(i, isCheckPlan, e.check)} />
                                 )
-                            })
-
-                            }  
+                            })}  
                         </div>
 
                         <br/>
@@ -110,11 +102,9 @@ export default function Todos() {
                             <label class="label">Protocolo</label>
                             {isCheckProt.map((e,i) => {
                                 return(
-                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheckOth(i, e.check)}/> 
+                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheck(i, isCheckProt, e.check)}/> 
                                 )
-                            })
-
-                            }
+                            })}
                         </div>
 
                         <br/>
@@ -123,10 +113,9 @@ export default function Todos() {
                             <label class="label">Contenido de</label>
                             {isCheckCont.map((e, i) => {
                                 return(
-                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheckCont(i,e.check)}/>
+                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheck(i, isCheckCont, e.check)}/>
                                 )
-                                })
-                            }   
+                                })}   
                         </div>
                     </div>
 
@@ -135,7 +124,7 @@ export default function Todos() {
                             <label class="label">Asesorías en:</label>
                             {isCheckAsen.map((e,i) => {
                                 return(
-                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheckAsen(i, e.check)} />
+                                    <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheck(i, isCheckAsen, e.check)} />
                                 )
                             })}
             
@@ -145,10 +134,15 @@ export default function Todos() {
             </StepBox>
             <hr/>
             <StepBox title="Servicios de Prensa y Audiovisual">
-                <div>
-                    <p>Hola soy nuevo</p>
-                    <p>ALOOO</p>
+                <div className="field">
+                <label class="label">Servicios de Prensa y Audiovisual:</label>
+                    {isCheckPrAu.map((e,i) => {
+                        return(
+                            <Checkbox label={e.value} index={i} toggleChange={() => onChangeCheck(i, isCheckPrAu, e.check)} />
+                        )
+                    })}
                 </div>
+                <Textarea label="Información para nota de Prensa"/>
             </StepBox>                    
         </div>
         
