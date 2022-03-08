f565a856753f1bb8739a6563b58ec8144389a62c
susan castillo <castillosusana55@gmail.com>
2022-03-08 11:12:10 -0500

    aded new archives

---
+++ client/pages/components/CheckQnty.js
@@ -0,0 +1,9 @@
+export default function CheckQnty({ item, index, toggleChange }) {
+    return(
+        <div className="column field is-one-fifth">
+            <div className="control">
+                <input type="number" className="input is-small"/>
+            </div>
+        </div>
+    )
+};
---
+++ client/pages/resume.js
@@ -0,0 +1,9 @@
+function Resume() {
+    return(
+        <div className="mt-6 pt-6">
+            <h1>soy resumen RESULTADOS</h1>
+        </div>
+    ) 
+}
+
+export default Resume
